a17adc7c8e1b676d47a107da8c41244a1bdd1b52
The Arcade Vaults (7) <_pc-vr@thearcadevaults.org.uk>
2022-01-29 12:38:33 +0000

    VR camera no longer shows player body

--- Roaches/Assets/Scripts/RigControllerVR.cs
+++ Roaches/Assets/Scripts/RigControllerVR.cs
@@ -13,14 +13,12 @@ public class RigControllerVR : MonoBehaviour
 
     public Vector3 headBodyOffset;
 
-    // Start is called before the first frame update
     void Start()
     {
-        headBodyOffset = transform.position - headConstraint.position;
+        headBodyOffset = headBodyOffset + (transform.position - headConstraint.position);
     }
 
-    // Update is called once per frame
-    void LateUpdate()
+    void FixedUpdate()
     {
         transform.position = headConstraint.position + headBodyOffset;
         transform.forward = Vector3.ProjectOnPlane(headConstraint.up, Vector3.up).normalized;
